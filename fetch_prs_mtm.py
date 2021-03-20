d79991c9daa7667efb14e1e30c8f31791e528279
Victoria Pantoja <vicpantojadoamaral@gmail.com>
2021-03-19 22:04:59 -0300

    Remove unmerged PRs from calculation

--- fetch_prs_mtm.py
+++ fetch_prs_mtm.py
@@ -26,44 +26,62 @@ def get_formatted_list_of_commits(commit_data):
 
 
 def format_prs_list(prs_list, use_time_before_review=False):
-    prs_list_with_hours = [
+    return [
         {
             "title": pr["title"],
             "author": get_author_login(pr),
             "created_at": extract_datetime_or_none(pr.get("createdAt")),
-            "merged_at": extract_datetime_or_none(pr.get("mergedAt")),
-            "closed_at": extract_datetime_or_none(pr.get("closedAt")),
+            "merged_at": extract_datetime_or_none(pr.get("mergedAt"))
+            if pr.get("mergedAt")
+            else None,
+            "closed_at": extract_datetime_or_none(pr.get("closedAt"))
+            if pr.get("closedAt")
+            else None,
             "commits": get_formatted_list_of_commits(pr.get("commits")),
         }
         for pr in prs_list
     ]
 
-    return prs_list_with_hours
+
+def get_merged_prs(formatted_prs_list):
+    merged_prs = []
+
+    for pr in formatted_prs_list:
+        if pr["merged_at"] is not None:
+            merged_prs.append(pr)
+    return merged_prs
 
 
 def call_mean_time_to_merge_statistics(
     start_date, end_date, include_hotfixes=False, use_time_before_review=False
 ):
     prs_list = fetch_prs_between(start_date, end_date)
-    valid_prs_list = filter_valid_prs(
-        prs_list, include_hotfixes=include_hotfixes)
+    valid_prs_list = filter_valid_prs(prs_list, include_hotfixes=include_hotfixes)
 
     formatted_prs_list = format_prs_list(valid_prs_list)
+    merged_prs = get_merged_prs(formatted_prs_list)
+
+    if not merged_prs:
+        return "There are no valid PRs to pull this data from, please select another timeframe"
 
     time_to_merge_list = []
-    for pr in formatted_prs_list:
-        first_commit_time = pr.get("commits")[0].get("commited_at")
-        merged_timedelta = pr.get("merged_at") - first_commit_time
+    for pr in merged_prs:
+        first_commit_time = pr["commits"][0]["commited_at"]
+        merged_timedelta = pr["merged_at"] - first_commit_time
         time_to_merge_list.append(merged_timedelta)
 
-    print(f"Mean: {format_timedelta(numpy.mean(time_to_merge_list))} hours")
-    print(f"Median: {format_timedelta(numpy.median(time_to_merge_list))}")
-    print(
-        f"95 percentile: {format_timedelta(numpy.percentile(time_to_merge_list, 95))} hours")
+    return f"""
+            Total PRs calculated: {len(merged_prs)}\n
+            Mean: {format_timedelta(numpy.mean(time_to_merge_list))}\n
+            Median: {format_timedelta(numpy.median(time_to_merge_list))}\n
+            95 percentile: {format_timedelta(numpy.percentile(time_to_merge_list, 95))}
+            """
 
 
-call_mean_time_to_merge_statistics(
-    start_date=arrow.get("2019-10-01"),
-    end_date=arrow.get("2019-10-31T23:59:59"),
-    include_hotfixes=False,
+print(
+    call_mean_time_to_merge_statistics(
+        start_date=arrow.get("2021-03-10"),
+        end_date=arrow.get("2021-03-12"),
+        include_hotfixes=False,
+    )
 )
