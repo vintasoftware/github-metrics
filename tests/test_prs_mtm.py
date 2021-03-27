1496e56ae419dc15f69e7617071f44adc36422f1
Victoria Pantoja <vicpantojadoamaral@gmail.com>
2021-03-27 13:07:55 -0300

    fix mtm test

--- tests/test_prs_mtm.py
+++ tests/test_prs_mtm.py
@@ -14,7 +14,7 @@ class TestPRsMTM(unittest.TestCase):
             {"merged_at": datetime.datetime(2021, 3, 25, 14, 28, 52)},
         ]
         merged_prs_list = get_merged_prs(prs_list)
-        self.assertEqual(len(merged_prs_list), 1)
+        self.assertEqual(merged_prs_list, [prs_list[1]])
 
     @mock.patch("fetch_prs_mtm.fetch_prs_between")
     def test_no_prs_to_calculate_mtm(self, mock_fetch_prs_between):
