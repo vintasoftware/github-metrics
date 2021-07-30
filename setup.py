91f65154de4678517cc20c54540324ce2ddc017d
Victoria Pantoja <vicpantojadoamaral@gmail.com>
2021-07-30 17:12:18 -0300

    Fill setup file

--- setup.py
+++ setup.py
@@ -7,9 +7,17 @@ with open("requirements.txt") as requirements_file:
         str(req) for req in parse_requirements(requirements_file.readlines())
     ]
 
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+
 setup(
     name="github_metrics",
     version="0.1",
+    description="Generate development metrics using github data for your project.",
+    url="https://github.com/vintasoftware/github-metrics",
+    author="Victoria Pantoja (Vinta Software)",
+    author_email="vicpantojadoamaral@gmail.com",
     python_requires=">=3.8",
     py_modules=["github_metrics"],
     install_requires=requirements,
@@ -17,4 +25,14 @@ setup(
         [console_scripts]
         github_metrics=github_metrics.run:cli
     """,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
 )
