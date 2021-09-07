7570992fc4980ee916287127382440d91d21de5b
Victoria Pantoja <vicpantojadoamaral@gmail.com>
2021-09-07 17:21:32 -0300

    Accepts other parameters

--- github_metrics/main.py
+++ github_metrics/main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import arrow
 from fastapi import FastAPI
 
@@ -13,18 +15,33 @@ def read_root():
 
 
 @app.get("/metrics/{metric}")
-def read_item(metric: str, start_date: str, end_date: str):
+def read_item(
+    metric: str,
+    start_date: str,
+    end_date: str,
+    include_hotfixes: Optional[bool] = False,
+    exclude_weekends: Optional[bool] = True,
+    exclude_authors: Optional[str] = None,
+    filter_authors: Optional[str] = None,
+):
     start = arrow.get(start_date)
     end = arrow.get(f"{end_date}T23:59:59")
 
     pr_list = fetch_prs_between(start, end)
 
-    data = get_time_to_merge_data(pr_list)
+    if metric == "ttm":
+        data = get_time_to_merge_data(
+            pr_list,
+            include_hotfixes,
+            exclude_authors,
+            filter_authors,
+            exclude_weekends,
+        )
 
-    return {
-        "metric": metric,
-        "mean": data["mean"],
-        "median": data["median"],
-        "percentile_95": data["percentile_95"],
-        "merged_prs": data["merged_prs"],
-    }
+        return {
+            "metric": metric,
+            "mean": data["mean"],
+            "median": data["median"],
+            "percentile_95": data["percentile_95"],
+            "merged_prs": data["merged_prs"],
+        }
